6668fc8d80f2b200ad302a69d62a8ed6e550e3e7
thiagodf <thiagodf@ciandt.com>
2019-08-31 12:45:39 -0300

    Metodo depositar

--- src/br/com/ciandt/thiagodf/Conta.java
+++ src/br/com/ciandt/thiagodf/Conta.java
@@ -9,9 +9,19 @@ package br.com.ciandt.thiagodf;
 public class Conta {
 	
 	private double saldo;
-
+	private double deposito;
+	
+	// Gets
 	public double getSaldo() {
 		return saldo;
 	}
-
+	public double getDeposito() {
+		return deposito;
+	}
+	
+	// Sets
+	public void setDeposito(double deposito) {
+		saldo = saldo + deposito;
+	}
+	
 }

4048fc292c03182040a67d287c5ec71a1a8b381c
thiagodf <thiagodf@ciandt.com>
2019-08-31 12:26:45 -0300

    Get saldo

--- src/br/com/ciandt/thiagodf/Conta.java
+++ src/br/com/ciandt/thiagodf/Conta.java
@@ -1,5 +1,17 @@
 package br.com.ciandt.thiagodf;
 
+/**
+ * Class Conta
+ * 
+ * @author thiagodf
+ *
+ */
 public class Conta {
+	
+	private double saldo;
+
+	public double getSaldo() {
+		return saldo;
+	}
 
 }
